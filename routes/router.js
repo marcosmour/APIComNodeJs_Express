3c8eecfdf9f59920c56e5af00803b976e706be4b
Marcos Moura <marcosmoura201@hotmail.com>
2023-03-20 14:52:23 -0300

    Criando a base das rotas

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 
 //Services router
 const servicesRouter = require('./services')
-
 router.use("/", servicesRouter)
+
+// Parties routes
+const partyRouter = require('./parties')
+router.use("/", partyRouter)
 
 module.exports = router;
